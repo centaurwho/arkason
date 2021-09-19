e9da2cd58ffabd9ebcd5c9bf3d1c737e5bd1a256
hakan sonmez <hakan.sn.hs@gmail.com>
2021-09-19 19:41:36 +0300

    Use behavior for dispatch in server handler

--- network/src/main/java/http/netty/HttpArkasonServerHandler.java
+++ network/src/main/java/http/netty/HttpArkasonServerHandler.java
@@ -7,6 +7,9 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.Behavior;
 
 import java.nio.charset.StandardCharsets;
 
@@ -16,76 +19,61 @@ import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HttpArkasonServerHandler extends SimpleChannelInboundHandler<HttpMessage> {
 
-    private HttpRequest request;
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpArkasonServerHandler.class);
+    private final Behavior<HttpObject, ChannelHandlerContext> behavior;
+
+    public HttpArkasonServerHandler() {
+        behavior = new Behavior<HttpObject, ChannelHandlerContext>()
+                .entry(HttpContent.class, this::onHttpContent)
+                .entry(HttpRequest.class, this::onHttpRequest);
+    }
 
     @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        super.channelReadComplete(ctx);
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    private void onHttpRequest(HttpRequest req, ChannelHandlerContext ctx) {
+        if (HttpUtil.is100ContinueExpected(req)) {
+            ctx.write(new DefaultFullHttpResponse(HTTP_1_1, CONTINUE));
+        }
+        boolean keepAlive = HttpUtil.isKeepAlive(req);
+
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK,
+                Unpooled.wrappedBuffer("hello world".getBytes(StandardCharsets.UTF_8)));
+        response.headers().set(CONTENT_TYPE, "text/plain");
+        response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+        if (!keepAlive) {
+            ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+        } else {
+            // For some reason KEEP_ALIVE is deprecated. So we cant use that constant.
+            response.headers().set(CONNECTION, "keep-alive");
+            ctx.write(response);
+        }
+    }
+
+    private void onHttpContent(HttpContent content, ChannelHandlerContext ctx) {
+        // TODO: make this a class variable
+        StringBuilder responseData = new StringBuilder();
+        responseData.append(RequestUtils.formatBody(content));
+
+        if (content instanceof LastHttpContent) {
+            LastHttpContent last = (LastHttpContent) content;
+            responseData.append(RequestUtils.prepareLastResponse(last));
+            ByteBuf responseBuf = Unpooled.copiedBuffer(responseData.toString().getBytes(StandardCharsets.UTF_8));
+            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, responseBuf);
+            ctx.write(response);
+        }
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpMessage msg) {
-        StringBuilder responseData = new StringBuilder();
-        if (msg instanceof HttpRequest req) {
-            this.request = req;
-            if (HttpUtil.is100ContinueExpected(req)) {
-                writeResponse(ctx);
-            }
-
-            responseData.append(RequestUtils.formatParams(req));
-        }
-        responseData.append(RequestUtils.evaluateDecoderResult(request));
-        if (msg instanceof HttpContent content) {
-            responseData.append(RequestUtils.formatBody(content));
-            responseData.append(RequestUtils.evaluateDecoderResult(request));
-
-            if (msg instanceof LastHttpContent last) {
-                responseData.append(RequestUtils.prepareLastResponse(last));
-                writeResponse(ctx, last, responseData);
-            }
-        }
-
-    }
-
-    private void writeResponse(ChannelHandlerContext ctx, LastHttpContent last, StringBuilder responseData) {
-        HttpResponseStatus status = determineStatus(last);
-        ByteBuf content = Unpooled.copiedBuffer(responseData.toString(), StandardCharsets.UTF_8);
-
-        FullHttpResponse httpResponse = new DefaultFullHttpResponse(HTTP_1_1, status, content);
-
-        httpResponse.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
-
-        boolean keepAlive = HttpUtil.isKeepAlive(request);
-        if (keepAlive) {
-            httpResponse.headers().setInt(CONTENT_LENGTH, httpResponse.content().readableBytes());
-            httpResponse.headers().set(CONNECTION, HttpHeaderValues.KEEP_ALIVE);
-        }
-
-        ctx.write(httpResponse);
-
-        if (!keepAlive) {
-            ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
-        }
-
-    }
-
-    private HttpResponseStatus determineStatus(LastHttpContent last) {
-        // TODO: Status logic here
-
-        if (last.decoderResult().isSuccess()) {
-            return OK;
-        } else {
-            return BAD_REQUEST;
-        }
-    }
-
-    private void writeResponse(ChannelHandlerContext ctx) {
-        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, CONTINUE, Unpooled.EMPTY_BUFFER);
-        ctx.write(response);
+        behavior.apply(msg, ctx);
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        super.exceptionCaught(ctx, cause);
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        LOGGER.error("Error in http handler.", cause);
+        ctx.close();
     }
 }
--- network/src/main/java/http/netty/util/HttpRequestBehavior.java
+++
@@ -1,8 +0,0 @@
-package http.netty.util;
-
-import io.netty.handler.codec.http.HttpMessage;
-import util.Behavior;
-
-public class HttpRequestBehavior extends Behavior<HttpMessage, Integer> {
-    // TODO: Empty for now. Possible to add specialized functions
-}
